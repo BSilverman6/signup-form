a926d1ba182bf596e39f28a06ea27bed928d95f7
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2024-01-11 09:08:32 -0700

    added password validation onsubmit event for form. Took away button disabling

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ passwordZ.forEach((item)=>item.addEventListener("keyup",()=> checkPassword()))
 
 function checkPassword(){
     let hasMatchWarning = document.querySelector('.no-PWD-match')==null? false:true;
-    if (passwordZ[0].value===passwordZ[1].value&&passwordZ[0].value.length>0&&passwordZ[1].value.length>0){
+    if (passwordZ[0].value===passwordZ[1].value&&passwordZ[0].value.length>0&&passwordZ[1].value.length>0&&hasMatchWarning){
         passwordZ[0].parentNode.removeChild(noMatchWarning);
         console.log("Warning Removed");
         hasMatchWarning=false;
@@ -19,10 +19,18 @@ function checkPassword(){
         hasMatchWarning=true;
     };
 
-    if (hasMatchWarning===true){
+    /*if (hasMatchWarning===true){
         submit.disabled=true;
     }else{
         submit.disabled=false;
-    };
+    };*/
 }
 
+function validatePasswords(){
+    console.log("validating Passwords")
+    if (passwordZ[0].value===passwordZ[1].value){
+        return true;
+    }else{
+        return false;
+    }
+}
