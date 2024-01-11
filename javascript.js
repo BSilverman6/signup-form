8d1b3f23583c807b5d845df86ec1f7c6c257869b
Bennett Silverman <152738859+BSilverman6@users.noreply.github.com>
2024-01-10 22:58:22 -0700

    added Javascript to enable and disable buttons with password Matching added CSS desktop styling touches

---
+++ javascript.js
@@ -0,0 +1,28 @@
+
+const passwordZ = document.querySelectorAll('input[type="password"');
+const noMatchWarning = document.querySelector('.no-PWD-match');
+const submit = document.querySelector(".form-submit");
+
+
+passwordZ.forEach((item)=>item.addEventListener("keyup",()=> checkPassword()))
+
+
+function checkPassword(){
+    let hasMatchWarning = document.querySelector('.no-PWD-match')==null? false:true;
+    if (passwordZ[0].value===passwordZ[1].value&&passwordZ[0].value.length>0&&passwordZ[1].value.length>0){
+        passwordZ[0].parentNode.removeChild(noMatchWarning);
+        console.log("Warning Removed");
+        hasMatchWarning=false;
+    }else if (passwordZ[0].value!==passwordZ[1].value&&!hasMatchWarning){
+        passwordZ[0].parentNode.appendChild(noMatchWarning);
+        console.log("Warning Added");
+        hasMatchWarning=true;
+    };
+
+    if (hasMatchWarning===true){
+        submit.disabled=true;
+    }else{
+        submit.disabled=false;
+    };
+}
+
